df1cf0ffbd5b97bb5b09ab53690e6e7a0b376bc6
chenyyyykun <56754139+chenyyyykun@users.noreply.github.com>
2023-08-07 21:57:39 +0800

    Add files via upload

---
+++ DIR.py
@@ -0,0 +1,182 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import timm
+import numpy as np
+from torch.autograd import Variable
+
+from einops import repeat, rearrange
+from einops.layers.torch import Rearrange
+
+from timm.models.layers import trunc_normal_
+from timm.models.vision_transformer import Block
+
+from model import ResNet18
+
+def random_indexes(size : int):
+    forward_indexes = np.arange(size)
+    np.random.shuffle(forward_indexes)
+    backward_indexes = np.argsort(forward_indexes)
+    return forward_indexes, backward_indexes
+
+def take_indexes(sequences, indexes):
+    return torch.gather(sequences, 0, repeat(indexes, 't b -> t b c', c=sequences.shape[-1]))
+
+class PatchShuffle(torch.nn.Module):
+    def __init__(self, ratio) -> None:
+        super().__init__()
+        self.ratio = ratio
+
+    def forward(self, patches : torch.Tensor):
+        T, B, C = patches.shape
+        remain_T = int(T * (1 - self.ratio))
+
+        indexes = [random_indexes(T) for _ in range(B)]
+        forward_indexes = torch.as_tensor(np.stack([i[0] for i in indexes], axis=-1), dtype=torch.long).to(patches.device)
+        backward_indexes = torch.as_tensor(np.stack([i[1] for i in indexes], axis=-1), dtype=torch.long).to(patches.device)  #乱序后的编号
+
+        patches = take_indexes(patches, forward_indexes)
+        patches_1 = patches[:remain_T]
+        patches_2 = patches[remain_T:]
+
+        return patches_1,patches_2, forward_indexes, backward_indexes
+
+class DIR_Encoder(torch.nn.Module):
+    def __init__(self,
+                 image_size=32,
+                 patch_size=2,
+                 emb_dim=192,
+                 num_layer=12,
+                 num_head=3,
+                 mask_ratio=0.5,
+                 ) -> None:
+        super().__init__()
+
+        self.cls_token = torch.nn.Parameter(torch.zeros(1, 1, emb_dim))
+        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_size // patch_size) ** 2, 1, emb_dim))
+        self.shuffle = PatchShuffle(mask_ratio)
+
+        self.patchify = torch.nn.Conv2d(3, emb_dim, patch_size, patch_size)
+
+        self.transformer = torch.nn.Sequential(*[Block(emb_dim, num_head) for _ in range(num_layer)])
+
+        self.layer_norm = torch.nn.LayerNorm(emb_dim)
+
+        self.init_weight()
+
+    def init_weight(self):
+        trunc_normal_(self.cls_token, std=.02)
+        trunc_normal_(self.pos_embedding, std=.02)
+
+    def forward(self, img):
+        patches = self.patchify(img)
+        patches = rearrange(patches, 'b c h w -> (h w) b c')
+        patches = patches + self.pos_embedding
+
+        patches_1,patches_2, _ , backward_indexes = self.shuffle(patches)
+
+        patches_1 = torch.cat([self.cls_token.expand(-1, patches_1.shape[1], -1), patches_1], dim=0)
+        patches_1 = rearrange(patches_1, 't b c -> b t c')
+        features_1 = self.layer_norm(self.transformer(patches_1))
+        features_1 = rearrange(features_1, 'b t c -> t b c')
+
+        patches_2 = torch.cat([self.cls_token.expand(-1, patches_2.shape[1], -1), patches_2], dim=0)
+        patches_2 = rearrange(patches_2, 't b c -> b t c')
+        features_2 = self.layer_norm(self.transformer(patches_2))
+        features_2 = rearrange(features_2, 'b t c -> t b c')
+
+        return features_1, features_2, backward_indexes
+
+class DIR_Decoder(torch.nn.Module):
+    def __init__(self,
+                 image_size=32,
+                 patch_size=2,
+                 emb_dim=192,
+                 num_layer=4,
+                 num_head=3,
+                 ) -> None:
+        super().__init__()
+
+        self.mask_token = torch.nn.Parameter(torch.zeros(1, 1, emb_dim))
+        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_size // patch_size) ** 2 + 1, 1, emb_dim))
+
+        self.transformer = torch.nn.Sequential(*[Block(emb_dim, num_head) for _ in range(num_layer)])
+
+        self.head = torch.nn.Linear(emb_dim, 3 * patch_size ** 2)
+        self.patch2img = Rearrange('(h w) b (c p1 p2) -> b c (h p1) (w p2)', p1=patch_size, p2=patch_size, h=image_size//patch_size)
+
+        self.init_weight()
+
+    def init_weight(self):
+        trunc_normal_(self.mask_token, std=.02)
+        trunc_normal_(self.pos_embedding, std=.02)
+
+    def forward(self, features, backward_indexes):
+        T = features.shape[0]
+        backward_indexes = torch.cat([torch.zeros(1, backward_indexes.shape[1]).to(backward_indexes), backward_indexes + 1], dim=0)
+        features = torch.cat([features, self.mask_token.expand(backward_indexes.shape[0] - features.shape[0], features.shape[1], -1)], dim=0)
+        features = take_indexes(features, backward_indexes)
+        features = features + self.pos_embedding
+
+        features = rearrange(features, 't b c -> b t c')
+        features = self.transformer(features)
+        features = rearrange(features, 'b t c -> t b c')
+        features = features[1:] # remove global feature
+
+        # patches = self.head(features)
+        # mask = torch.zeros_like(patches)
+        # mask[T:] = 1
+        # mask = take_indexes(mask, backward_indexes[1:] - 1)
+        # img = self.patch2img(patches)
+        # mask = self.patch2img(mask)
+
+        return features
+
+class DIR_ViT(torch.nn.Module):
+    def __init__(self,
+                 image_size=32,
+                 patch_size=2,
+                 emb_dim=192,
+                 encoder_layer=12,
+                 encoder_head=3,
+                 decoder_layer=4,
+                 decoder_head=3,
+                 mask_ratio=0.5,
+                 ) -> None:
+        super().__init__()
+
+        self.patchify = torch.nn.Conv2d(3, emb_dim, patch_size, patch_size)
+        self.encoder = DIR_Encoder(image_size, patch_size, emb_dim, encoder_layer, encoder_head, mask_ratio)
+        self.decoder = DIR_Decoder(image_size, patch_size, emb_dim, decoder_layer, decoder_head)
+        # self.head = torch.nn.Linear(emb_dim, 3 * patch_size ** 2)
+        self.patch2img = Rearrange('(h w) b (c p1 p2) -> b c (h p1) (w p2)', p1=patch_size, p2=patch_size, h=image_size//patch_size)
+        self.img2patch = Rearrange('b c (h p1) (w p2) -> (h w) b (c p1 p2)',p1=patch_size, p2=patch_size, h=image_size//patch_size)
+        self.b2c = Rearrange('c b f ->b c f')
+        self.c2b = Rearrange('b c f ->c b f')
+        self.conv1 = torch.nn.Conv1d(256,256,16,stride=16)
+
+    def Loss(self,img,pred):
+        loss = (pred - img)**2
+        loss = loss.mean(dim=-1)
+        loss = loss.sum()/(loss.size()[0]*loss.size()[1])  #200:batch_size; 256:patches_num
+        return loss
+
+    def forward(self, img_adv, img_nat):
+        features_1, features_2, backward_indexes = self.encoder(img_adv)
+        pred_1 = self.decoder(features_1,  backward_indexes)
+        pred_2 = self.decoder(features_2,  backward_indexes)
+        pred_all = (pred_1 +pred_2)/2
+        # pred_patches = self.head(pred_all)
+        pred_b2c = self.b2c(pred_all)
+        pred_conv = self.conv1(pred_b2c)
+        pred_c2b = self.c2b(pred_conv)
+        # pred = pred_c2b+pred_patches
+        pred = pred_c2b
+        img_patches = self.img2patch(img_nat)
+        loss = self.Loss(img_patches,pred)
+        img = self.patch2img(pred)
+        return img, loss
+
+
+
+
---
+++ pre_training.py
@@ -0,0 +1,110 @@
+import numpy as np
+from PIL import Image
+import argparse
+from utils import setup_seed
+from os import listdir
+import math
+from tqdm import tqdm
+import sys
+import pickle
+import random
+import torch
+import cv2
+from model import ResNet18
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from model_try import *
+# from model import *
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--batch_size', type=int, default=250)
+    parser.add_argument('--max_device_batch_size', type=int, default=512)
+    parser.add_argument('--base_learning_rate', type=float, default=1.5e-4)
+    parser.add_argument('--weight_decay', type=float, default=0.05)
+    parser.add_argument('--mask_ratio', type=float, default=0.5)
+    parser.add_argument('--total_epoch', type=int, default=1000)
+    parser.add_argument('--warmup_epoch', type=int, default=200)
+    # parser.add_argument('--model_path', type=str, default='/data/DIR-main/model/average_1.4_1000.pt')  
+    parser.add_argument('--num_steps',type=int,default=20)              
+    parser.add_argument('--epsilon', type=float, default=0.0314,help='perturbation')
+    parser.add_argument('--step_size', type=float, default=0.003,help='perturb step size')
+
+    return parser
+
+
+def unpickle(file):
+    read = open(r"/data/dataset/cifar-ours/"+file,'rb')
+    dict = pickle.load(read)
+    return dict
+
+data_set=["nature_sample.pkl","adversarial_sample.pkl","nature_sample_test.pkl","adversarial_sample_test.pkl"]
+nat_sample = unpickle(data_set[0])
+adv_sample = unpickle(data_set[1])
+nat_sample_test = unpickle(data_set[2])
+adv_sample_test = unpickle(data_set[3])
+
+
+def main(args):
+    setup_seed(args.seed)
+    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+    # device_ids = [2,3]
+    model = DIR_ViT(mask_ratio=args.mask_ratio)
+    model = torch.load('/data/DIR-main/model/average_1.4.pt')
+    # model = torch.nn.DataParallel(model,device_ids=device_ids)
+    model = model.to(device)
+    optim = torch.optim.AdamW(model.parameters(), lr=args.base_learning_rate * args.batch_size / 256, betas=(0.9, 0.95), weight_decay=args.weight_decay)
+    lr_func = lambda epoch: min((epoch + 1) / (args.warmup_epoch + 1e-8), 0.5 * (math.cos(epoch / args.total_epoch * math.pi) + 1))
+    lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func, verbose=True)
+
+    optim.zero_grad()
+    for e in range(290,args.total_epoch):
+        model.train()
+        losses = []
+        losses_test = []
+        nat_data=nat_sample[b'data']
+        adv_data=adv_sample[b'data']
+        nat_data = nat_data.reshape(50000,3,32,32)
+        adv_data = adv_data.reshape(50000,3,32,32)
+        batches = tqdm(range(int(np.shape(nat_data)[0]/args.batch_size)))
+        for b in batches:
+            data_input = torch.tensor(np.float32(((adv_data[b*args.batch_size:(b+1)*args.batch_size,:]/255)-0.5)*2))
+            data_output = torch.tensor(np.float32(((nat_data[b*args.batch_size:(b+1)*args.batch_size,:]/255)-0.5)*2))
+            data_input, data_output = data_input.to(device), data_output.to(device)
+            _, loss = model(data_input,data_output)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+            losses.append(loss.item())
+            # print('batch[{}/{}]'.format(b+1,int(np.shape(nat_data)[0]/args.batch_size)))
+        lr_scheduler.step()
+        avg_loss = sum(losses) / len(losses)
+        # writer.add_scalar('DIR_loss', avg_loss, global_step=e)
+        print(f'In epoch {e}, average traning loss is {avg_loss}.')
+
+        ''' visualize the first 16 predicted images on val dataset'''
+        model.eval()
+        with torch.no_grad():
+            nat_data_test=nat_sample_test[b'data']
+            adv_data_test=adv_sample_test[b'data']
+            nat_data_test = nat_data_test.reshape(10000,3,32,32)
+            adv_data_test = adv_data_test.reshape(10000,3,32,32)
+            for b in range(int(np.shape(nat_data_test)[0]/args.batch_size)):
+                data_input = torch.tensor(np.float32(((adv_data_test[b*args.batch_size:(b+1)*args.batch_size,:]/255)-0.5)*2))
+                data_output = torch.tensor(np.float32(((nat_data_test[b*args.batch_size:(b+1)*args.batch_size,:]/255)-0.5)*2))
+                data_input, data_output = data_input.to(device), data_output.to(device)
+                _, loss_test = model(data_input, data_output)
+            #     loss_test = torch.mean((predicted_img - data_output) ** 2 )
+                losses_test.append(loss_test.item())
+            avg_loss_test = sum(losses_test) / len(losses_test)
+            print(f'In epoch {e}, average traning loss in test is {avg_loss_test}.')
+        
+        ''' save model '''
+        torch.save(model, args.model_path)
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    main(args)
